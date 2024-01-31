e558190f6e95101c65f664bd225053e4732cee72
DronCode <alexandrleutin@gmail.com>
2024-01-31 20:18:26 +0300

    [CI] Another try to fix setup.py and upload assets to PyPI

--- Extension/setup.py
+++ Extension/setup.py
@@ -1,42 +1,47 @@
 from setuptools import setup
-from typing import List
-from dataclasses import dataclass
+from typing import Dict
 from os import getenv
 
 
-@dataclass
-class PlatformTargetDesc:
-    id: str
-    dest_folder: str
-    native_ext: str
+known_os_ext: Dict[str, str] = {
+    'linux': 'so',
+    'macos': 'so',
+    'windows': 'pyd'
+}
+
+# Read env
+target_version: str = getenv('RG3_DEPLOY_VERSION', 'none')
+target_os: str = getenv('RG3_DEPLOY_OS', 'none')
+
+if target_version == 'none':
+    raise RuntimeError("RG3_DEPLOY_VERSION not specified!")
+
+if target_os not in known_os_ext:
+    raise RuntimeError(f"RG3_DEPLOY_OS not specified or unknown ext for '{target_os}'")
+
+platform_ext: str = known_os_ext[target_os]
+
+print("*************************************************")
+print(f"RG3 Deploy for version {target_version} target os {target_os} target ext {platform_ext}")
+print("*************************************************")
 
 
-target_version: str = getenv('RG3_DEPLOY_VERSION', '0.0.1')
-platforms: List[PlatformTargetDesc] = [PlatformTargetDesc(id="Windows", dest_folder='RG3_Windows', native_ext='pyd'),
-                                       PlatformTargetDesc(id="Linux", dest_folder='RG3_Linux', native_ext='so')]
-platform_desc: PlatformTargetDesc
-
-print(f" *** RG3 Package builder for {target_version} (0.0.1 default) *** ")
-
-for platform_desc in platforms:
-    print(f"Run for platform {platform_desc.id} (rg3py.{platform_desc.native_ext})")
-    setup(
-        name='rg3py',
-        version=target_version,
-        author='DronCode',
-        author_email='alexandrleutin@gmail.com',
-        description='RG3 is a C/C++ analyzer framework',
-        long_description='RG3 is a C/C++ analyzer framework',
-        packages=['rg3py'],
-        package_data={'rg3py': [f'{platform_desc.dest_folder}/rg3py.{platform_desc.native_ext}',
-                                f'{platform_desc.dest_folder}/rg3py.pyi']},
-        classifiers=[
-            'Programming Language :: Python :: 3',
-            'License :: OSI Approved :: MIT License',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python :: Implementation :: CPython',
-        ],
-        python_requires='>=3.10',
-        platforms=['Windows', 'Linux'],
-    )
+setup(
+    name='rg3py',
+    version=target_version,
+    author='DronCode',
+    author_email='alexandrleutin@gmail.com',
+    description='RG3 is a C/C++ analyzer framework',
+    long_description='RG3 is a C/C++ analyzer framework',
+    packages=['rg3py'],
+    package_data={'rg3py': [f'rg3py.{platform_ext}', 'rg3py.pyi']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    python_requires='>=3.10',
+    platforms=['Windows', 'Linux'],
+)
